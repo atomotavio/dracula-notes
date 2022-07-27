b9b0dda2b3163523a76999eba1a61ff311f9c502
atomotavio <atomotavio@gmail.com>
2022-07-27 20:05:35 -0300

    feature: data is stored in LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style/style.css";
 
+// getting the values of local storage
+const getDatafromLS=()=>{
+  const data = localStorage.getItem('notes');
+  if(data){
+    return JSON.parse(data);
+  }
+  else{
+    return []
+  }
+}
+
 function App() {
+
+  const [notes, setNotes]=useState(getDatafromLS());
+
+  const [title, setTitle]=useState('');
+  const [content, setContent]=useState('');
+
+  const handleAddNoteSubmit=(e)=>{
+    e.preventDefault();
+
+    let note={
+      title,
+      content
+    }
+    setNotes([...notes,note]);
+    setTitle('');
+    setContent('');
+  }
+
+  const deleteNote=(title)=>{
+    const filteredNotes=notes.filter((element,index)=>{
+      return element.title !== title
+    })
+    setNotes(filteredNotes);
+  }
+
+  useEffect(()=>{
+    localStorage.setItem('notes',JSON.stringify(notes));
+  },[notes])
+
   return (
     <>
     <nav>
       <h1>Dracula Notes</h1>
     </nav>
 
-    <form>
-    <label for="ftitle">Title:</label><br />
-    <input type="text" name="ftitle"></input><br />
-    <label for="message">Content:</label><br />
-    <textarea name="message" rows="10" cols="30" placeholder="What do you want to say?"></textarea> <br />
+    <form onSubmit={handleAddNoteSubmit}>
+    <label>
+        Title:
+        <input type="text" name="title" value={title} onChange={(e)=> setTitle(e.target.value)} required/>
+    </label>
+    <label>
+          Content:
+          <textarea value={content} onChange={(e) => setContent(e.target.value)} required />
+    </label>
     <button type="submit">Add New Note</button>
     </form>
     
